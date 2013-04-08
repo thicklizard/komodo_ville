1059e9c2bc25bc249f0a9080a6ee03d51e17a9b8
thicklizard <thicklizard@gmail.com>
2013-04-08 13:13:00 -0500

    komodo 3 ville

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -2560,11 +2560,7 @@ static struct msm_bus_vectors grp3d_low_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-<<<<<<< HEAD
 		.ib = KGSL_CONVERT_TO_MBPS(2400),  //300 MHz GPU
-=======
-		.ib = KGSL_CONVERT_TO_MBPS(2048),
->>>>>>> a07ff5d813f54eeec8bf25a5441589477e101aa9
 	},
 };
 
@@ -2573,11 +2569,7 @@ static struct msm_bus_vectors grp3d_nominal_low_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-<<<<<<< HEAD
 		.ib = KGSL_CONVERT_TO_MBPS(3200),  //400 MHz GPU
-=======
-		.ib = KGSL_CONVERT_TO_MBPS(2656),
->>>>>>> a07ff5d813f54eeec8bf25a5441589477e101aa9
 	},
 };
 
@@ -2642,11 +2634,7 @@ static struct msm_bus_vectors grp2d0_nominal_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-<<<<<<< HEAD
 		.ib = KGSL_CONVERT_TO_MBPS(2400),  //300 MHz 
-=======
-		.ib = KGSL_CONVERT_TO_MBPS(1000),
->>>>>>> a07ff5d813f54eeec8bf25a5441589477e101aa9
 	},
 };
 
@@ -2655,11 +2643,7 @@ static struct msm_bus_vectors grp2d0_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-<<<<<<< HEAD
 		.ib = KGSL_CONVERT_TO_MBPS(2560),  //320MHz GPU = 2400 Mbps
-=======
-		.ib = KGSL_CONVERT_TO_MBPS(2048),
->>>>>>> a07ff5d813f54eeec8bf25a5441589477e101aa9
 	},
 };
 
@@ -2698,11 +2682,7 @@ static struct msm_bus_vectors grp2d1_nominal_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-<<<<<<< HEAD
 		.ib = KGSL_CONVERT_TO_MBPS(2400),  //300 MHz GPU
-=======
-		.ib = KGSL_CONVERT_TO_MBPS(1000),
->>>>>>> a07ff5d813f54eeec8bf25a5441589477e101aa9
 	},
 };
 
@@ -2711,11 +2691,7 @@ static struct msm_bus_vectors grp2d1_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-<<<<<<< HEAD
 		.ib = KGSL_CONVERT_TO_MBPS(2560),  //320MHz GPU = 2400 Mbps
-=======
-		.ib = KGSL_CONVERT_TO_MBPS(2048),
->>>>>>> a07ff5d813f54eeec8bf25a5441589477e101aa9
 	},
 };
 
@@ -2775,17 +2751,17 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		{
 			.gpu_freq = 512000000,
 			.bus_freq = 4,
-			.io_fraction = 33,
+			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 400000000,
 			.bus_freq = 3,
-			.io_fraction = 50
+			.io_fraction = 0
 		},
 		{
 			.gpu_freq = 300000000,
 			.bus_freq = 2,
-			.io_fraction = 100,
+			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 266667000,
@@ -2798,7 +2774,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.max_level= 2,
+	.max_level= 0,
 	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
@@ -2855,28 +2831,20 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwrlevel = {
 		{
 			.gpu_freq = 320000000,
-			.bus_freq = 4,
+			.bus_freq = 2,
 		},
 		{
 			.gpu_freq = 266667000,
-			.bus_freq = 3,
+			.bus_freq = 1,
 		},
 		{
-		 	.gpu_freq = 200000000,
- 			.bus_freq = 2,
- 		},
- 		{
- 			.gpu_freq = 96000000,
- 			.bus_freq = 1,
- 		},
- 		{
  			.gpu_freq = 27000000,
  			.bus_freq = 0,
 		},
 	},
-	.init_level = 1,
-	.max_level= 2,
-	.num_levels = 5,
+	.init_level = 0,
+	.max_level= 0,
+	.num_levels = 3,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
 	.nap_allowed = true,
@@ -2930,34 +2898,22 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwrlevel = {
 		{
 			.gpu_freq = 320000000,
-			.bus_freq = 4,
+			.bus_freq = 2,
 		},
 		{
 			.gpu_freq = 266667000,
-			.bus_freq = 3,
+			.bus_freq = 1,
 		},
 		{
-		 	.gpu_freq = 200000000,
- 			.bus_freq = 2,
- 		},
- 		{
- 			.gpu_freq = 96000000,
- 			.bus_freq = 1,
- 		},
- 		{
  			.gpu_freq = 27000000,
  			.bus_freq = 0,
 		},
 	},
-	.init_level = 1,
-	.max_level= 2,
-	.num_levels = 5,
+	.init_level = 0,
+	.max_level= 0,
+	.num_levels = 3,
 	.set_grp_async = NULL,
-<<<<<<< HEAD
-	.idle_timeout = HZ/5,
-=======
 	.idle_timeout = HZ/12,
->>>>>>> a07ff5d813f54eeec8bf25a5441589477e101aa9
 	.nap_allowed = true,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
